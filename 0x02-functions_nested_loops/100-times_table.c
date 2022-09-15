851adbceb051d3186d8356ef8db56865b21683cd
Your Name <you@example.com>
2022-09-14 21:30:55 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints a multiplication table
+ * @n: The number to be treated
+ *
+ * Return: Number matrix
+ */
+void print_times_table(int n)
+{
+	int i, j, product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; ++i)
+		{
+			for (j = 0; j <= n; ++j)
+			{
+				product = i * j;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product < 100)
+						_putchar(' ');
+					if (product < 10)
+						_putchar(' ');
+				}
+				if (product >= 100)
+				{
+					_putchar('0' + product / 100);
+					_putchar('0' + (product % 100) / 10);
+					_putchar('0' + (product % 10));
+				}
+				else if (product <= 9)
+				{
+					_putchar('0' + product);
+				}
+				else
+				{
+					_putchar('0' + product / 10);
+					_putchar('0' + product % 10);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
